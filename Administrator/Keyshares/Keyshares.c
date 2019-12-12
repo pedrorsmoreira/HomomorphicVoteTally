0ae082ccd853798a9317a74f733fb8ed87ce5d12
diogoegrodrigues <diogoegrodrigues@gmail.com>
2019-12-12 00:10:02 +0000

    pc -> vbox

--- Administrator/Keyshares/Keyshares.c
+++ Administrator/Keyshares/Keyshares.c
@@ -3,40 +3,35 @@
 #include <string.h>
 #include "sss/sss.h"
 #include "sss/randombytes.h"
-#include <assert.h>
+//#include <assert.h>
 
-int main(int argc, char *argv[]) {
-	// Recives in argv[1] number of shares to create and argv[2] the threshold
-
-	int n_shares = atoi(argv[1]);
-	int thershold = atoi(argv[2]);
-	char pass[100];
+int main(int argc, char *argv[])
+{
+	uint8_t data[sss_MLEN];
+	int numberOfShares = atoi(argv[1]); // Receives the number of shares to create
+	int thershold = atoi(argv[2]); // Receives the defined threshold
 	char fileName[20];
-	FILE *fp;
+	sss_Share shares[numberOfShares];
 	
 	//rb - non text files
-	fp = fopen("../pass.txt", "rb");
-	//64 bits pass
-	for(int i = 0; i < 64; i++)
-		pass[w] = fgetc(fp);
-	pass[64]='\0';
+	fp = fopen("./pass.txt", "rb");
+	//64 bytess pass
+	for(int i = 0; i < sss_MLEN; i++)
+		data[i] = fgetc(fp);
+	data[64] = '\0';
 	fclose(fp);
 
-	sss_Share shares[n_shares];
-	
 	// Split the secret into shares
-	sss_create_shares(shares, pass, n_shares, thershold);
+	sss_create_shares(shares, data, numberOfShares, thershold);
 
-	for (int i = 0; i < n_shares; i++) 
-	{
+	for (int i = 0; i < numberOfShares; i++) {
 		snprintf(fileName, sizeof(fileName), "share%d.txt", i+1);
-
-		fp = fopen(fileName, "w");
-
+		fp = fopen(fileName, "wb");
 		for (int j = 0; j< sss_SHARE_LEN; j++)
 			fputc(shares[i][j], fp);
 		fputc('\0', fp);
 		fclose(fp);
-	}	
+	}
+
 	return 0;
 }
