4ad2d0c5f4518c3d8e7db465d4a8312515cac4b2
diogoegrodrigues <diogoegrodrigues@gmail.com>
2019-12-12 18:39:42 +0000

    pc -> vbox : Testing Administrator

--- Administrator/cleanup.sh
+++ Administrator/cleanup.sh
@@ -8,7 +8,7 @@ rm rootCA.key rootCA.crt
 
 cd ElectionKey
 rm -Rf CMakeFiles
-rm CMakeCache.txt cmake_install.cmake CMakeLists.txt
+rm CMakeCache.txt cmake_install.cmake
 rm electionKey electionPublicKey.dat electionPublicKey.sign electionSecretKey.dat.enc
 cd ..
 
@@ -18,6 +18,6 @@ cd ..
 
 cd Weights
 rm -Rf CMakeFiles
-rm CMakeCache.txt cmake_install.cmake CMakeLists.txt
+rm CMakeCache.txt cmake_install.cmake
 rm weights
 cd ..
