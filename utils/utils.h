97395f64a054f54d00034189a74f77812735a82a
irmaoshiva <rsmoreira.pedro@gmail.com>
2019-12-13 09:33:11 +0000

    aaa

--- utils/utils.h
+++ utils/utils.h
@@ -2,6 +2,31 @@
 #define UTILS
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#define BALLOT_BOX              "../BallotBox"
+
+//root CA certificate
+#define ROOT_CRT_FILE           "rootCA.crt"
+
+#define VOTER_CRT               "voter" //append ID
+#define VOTER_CRT_EXTENSION     ".crt"
+
+#define ELEC_KEY                "electionPublicKeyFile.dat"
+#define ELEC_KEY_SIGNED         "electionPublicKeyFile.sign"
+
+#define VOTE_INPUT              "input.txt"
+#define VOTE_INPUT_SIGNED       "input.sign"
+
+//name of the directory that will contain the vote
+#define VOTE_DIR                "Vote" //vote counter will be appended
+
+#define VOTE_ENCRYPTED          "vote.seal"
+#define VOTE_SIGNED             "vote.sign"
+
+
 
 void print(std::string& s);
 
